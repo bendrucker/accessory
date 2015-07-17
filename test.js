cd8f9f43d4bb07efbb70e7aa937ebcd1d87ef605
Ben Drucker <bvdrucker@gmail.com>
2015-07-17 11:24:58 -0400

    Add failing tests for function calls

--- test.js
+++ test.js
@@ -3,10 +3,19 @@
 var test = require('tape')
 var access = require('./')
 
-test(function (t) {
+test('accessors', function (t) {
   t.equal(access('window', 'foo'), "window['foo']")
   t.equal(access('window', 'foo-bar'), "window['foo-bar']")
   t.equal(access('window', 'foo.bar'), "window['foo']['bar']")
   t.equal(access('window', 'foo\\.bar'), "window['foo.bar']")
+  t.end()
+})
+
+test('calls', function (t) {
+  t.equal(access('window', 'foo()'), "window['foo']()")
+  t.equal(access('window', 'foo(bar)'), "window['foo'](bar)")
+  t.equal(access('window', "foo('bar')"), "window['foo']('bar')")
+  t.equal(access('window', 'foo\()'), "window['foo()']")
+  t.equal(access('window', 'foo(bar).baz'), "window['foo'](bar)['baz']")
   t.end()
 })
