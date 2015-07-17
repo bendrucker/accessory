df9794035fa9146a8db81449974334147b6e6030
Ben Drucker <bvdrucker@gmail.com>
2015-07-17 11:55:23 -0400

    Use balanced-match to detect and parse function calls

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ test('calls', function (t) {
   t.equal(access('window', 'foo()'), "window['foo']()")
   t.equal(access('window', 'foo(bar)'), "window['foo'](bar)")
   t.equal(access('window', "foo('bar')"), "window['foo']('bar')")
-  t.equal(access('window', 'foo\()'), "window['foo()']")
+  t.equal(access('window', 'foo(bar)(baz)(qux)'), "window['foo'](bar)(baz)(qux)")
   t.equal(access('window', 'foo(bar).baz'), "window['foo'](bar)['baz']")
   t.end()
 })
