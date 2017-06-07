2a1726f32c9b9694f63a95fbf4716a9ca76f99fc
Esref Durna <edurna@gmail.com>
2017-06-07 01:35:18 +0000

    eslint fixes: index.js    2:1   warning  More than 0 blank lines not allowed           no-multiple-empty-lines    7:1   warning  More than 0 blank lines not allowed           no-multiple-empty-lines    8:41  warning  Unexpected space before function parentheses  space-before-function-paren   11:1   warning  More than 0 blank lines not allowed           no-multiple-empty-lines   12:20  warning  Unexpected space before function parentheses  space-before-function-paren   14:18  warning  Unexpected space before function parentheses  space-before-function-paren
    ✖ 6 problems (0 errors, 6 warnings)

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 'use strict'
-
 var split = require('dot-parts')
 var balanced = require('balanced-match')
 var ap = require('ap')
 var findCall = ap.partial(balanced, '(', ')')
-
-module.exports = function createAccessor (source, path) {
+module.exports = function createAccessor(source, path) {
   return split(path).reduce(accumulate, source)
 }
-
-function accumulate (statement, property) {
+function accumulate(statement, property) {
   var callString = ''
-  function append (body) {
+  function append(body) {
     callString += '(' + body + ')'
   }
   var call = findCall(property)
